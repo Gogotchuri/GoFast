d030fae0209ede0a91ab070a2639cb73bbb2d304
lkito <lkito@freeuni.edu.ge>
2020-06-10 22:35:53 +0400

    added sign in/up page routing for future testing

--- resources/js/modules/auth/routes.module.js
+++ resources/js/modules/auth/routes.module.js
@@ -1,1 +1,15 @@
-export default [];
+import SignIn from "@views/auth/SignIn.vue";
+import SignUp from "@views/auth/SignUp.vue";
+
+export default [
+    {
+        path: "/sign-in",
+        name: "SignIn",
+        component: SignIn
+    },
+    {
+        path: "/sign-up",
+        name: "SignUp",
+        component: SignUp
+    }
+];
--- resources/views/partials/MainNav.vue
+++ resources/views/partials/MainNav.vue
@@ -4,6 +4,12 @@
             <router-link :to="{name: 'Home'}">
                 Home
             </router-link>
+            <router-link :to="{name: 'SignIn'}">
+                Sign in
+            </router-link>
+            <router-link :to="{name: 'SignUp'}">
+                Sign up
+            </router-link>
         </div>
     </nav>
 </template>
