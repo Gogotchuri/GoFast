df8feda0d50cf7fc8eeaa85628a72976a6d96958
Gega Darakhvelidze <gdara17@freeuni.edu.ge>
2020-07-26 21:33:43 +0400

    paginator service

--- app/services/pagination/paginator.go
+++ app/services/pagination/paginator.go
@@ -1,6 +1,8 @@
 package pagination
 
 import (
+	"math"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -24,5 +26,47 @@ type Paginator struct {
 
 /**Paginate given query and destination of model array returns paginator*/
 func Paginate(p *Query, dest interface{}, scan bool) *Paginator {
-	return nil
+	db := p.DB
+	//Paginator to return
+	paginator := &Paginator{}
+
+	if p.CurrentPage < 1 {
+		p.CurrentPage = 1
+	}
+	if p.PerPage == 0 {
+		p.PerPage = 10
+	}
+	//Order database
+	for _, o := range p.OrderBy {
+		db = db.Order(o)
+	}
+	//Get total records count
+	if scan {
+		db.Count(&paginator.TotalRecords)
+	} else {
+		db.Model(dest).Count(&paginator.TotalRecords)
+	}
+	//Calculate offset
+	paginator.Offset = (p.CurrentPage - 1) * p.PerPage
+	//Write into destination and append array elements to paginator records
+	if scan {
+		db.Limit(p.PerPage).Offset(paginator.Offset).Scan(dest)
+	} else {
+		db.Limit(p.PerPage).Offset(paginator.Offset).Find(dest)
+	}
+	paginator.Records = dest
+	paginator.CurrentPage = p.CurrentPage
+	paginator.PerPage = p.PerPage
+	paginator.TotalPages = int(math.Ceil(float64(paginator.TotalRecords) / float64(p.PerPage)))
+	//Set previous page
+	paginator.PrevPage = p.CurrentPage - 1
+	if p.CurrentPage == 1 {
+		paginator.PrevPage = p.CurrentPage
+	}
+	//Set next page
+	paginator.NextPage = p.CurrentPage + 1
+	if p.CurrentPage == paginator.TotalPages {
+		paginator.NextPage = p.CurrentPage
+	}
+	return paginator
 }
